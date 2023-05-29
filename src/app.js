98ae5c97e248997ee4d9a528d3d241a4faf817b5
Tcm68 <131905512+Tcm68@users.noreply.github.com>
2023-05-29 20:44:54 +0000

    added excuse functions

--- src/app.js
+++ src/app.js
@@ -5,7 +5,50 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function generateExcuse() {
+  let who = [
+    "The dog",
+    "My grandma",
+    "His turtle",
+    "My bird",
+    "Aliens",
+    "The neighbor"
+  ];
+  let action = ["ate", "peed on", "crushed", "broke", "hid", "burned"];
+  let what = [
+    "my homework",
+    "the keys",
+    "the car",
+    "the laptop",
+    "the project files"
+  ];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying",
+    "while I was sleeping"
+  ];
+
+  const randomWhoIndex = Math.floor(Math.random() * who.length);
+  const randomActionIndex = Math.floor(Math.random() * action.length);
+  const randomWhatIndex = Math.floor(Math.random() * what.length);
+  const randomWhenIndex = Math.floor(Math.random() * when.length);
+
+  const excuse =
+    who[randomWhoIndex] +
+    " " +
+    action[randomActionIndex] +
+    " " +
+    what[randomWhatIndex] +
+    " " +
+    when[randomWhenIndex];
+  return excuse;
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const generatedExcuse = generateExcuse();
+  const excusePlaceholder = document.getElementById("excuse");
+  excusePlaceholder.textContent = generatedExcuse;
 };
